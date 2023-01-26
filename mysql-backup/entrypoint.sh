86393efa556f15038eb6a27aaf5a5322c0e33af6
vokoun-imatic <radek.vokoun@imatic.cz>
2023-01-26 20:37:27 +0100

    fix env variables for cron, add log output from script

--- mysql-backup/entrypoint.sh
+++ mysql-backup/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# add cron file
 if [ ! -f /etc/cron.d/mysql-backup ]; then
         # Add cron.d file
-        echo "${CRON_TIME} root /etc/imt-sql-backup/mysql-backup.sh" > /etc/cron.d/mysql-backup
+        echo "${CRON_TIME} root /etc/imt-sql-backup/mysql-backup.sh >> /var/log/mysql-backup.log 2>&1" > /etc/cron.d/mysql-backup
 fi
 
+# make env variables available in cron jobs
+env > /etc/environment
+
+#start cron service and keep container running
 service cron start
 sleep infinity
