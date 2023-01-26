16ea74813f38c2f856031daf5627b1dc102f21c6
vokoun-imatic <radek.vokoun@imatic.cz>
2023-01-26 20:36:39 +0100

    fix capitalization of vars, add default values for conn vars

--- mysql-backup/mysql-backup.sh
+++ mysql-backup/mysql-backup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-MYSQL_CONN_STRING="-h $MYSQL_HOST -u $MYSQL_USER -p$MYSQL_PASSWORD -P $MYSQL_PORT"
-OUTPUTDIR="/var/backups/mysql"
-MYSQLDUMP="/usr/bin/mysqldump"
-MYSQL="/usr/bin/mysql"
+mysql_host=${MYSQL_HOST:-"127.0.0.1"}
+mysql_user=${MYSQL_USER:-"root"}
+mysql_password=${MYSQL_PASSWORD:-"root"}
+mysql_port=${MYSQL_PORT:-3306}
 
-EXTENSION="dump"
-REMOVEDUPLICATE=1
+mysql_conn_string="-h $mysql_host -u $mysql_user -p$mysql_password -P $mysql_port"
+outputdir="/var/backups/mysql"
+
+extension="dump"
+removeduplicate=1
 
 renice -n 10 --pid $$ &>/dev/null
 
@@ -16,7 +19,7 @@ renice -n 10 --pid $$ &>/dev/null
 set -eu
 
 function mygrants () {
-  mysql $MYSQL_CONN_STRING $@ -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | \
+  mysql $mysql_conn_string $@ -N -e "SELECT DISTINCT CONCAT('SHOW GRANTS FOR \'', user, '\'@\'', host, '\';') AS query FROM mysql.user" | \
   sed 's/\(GRANT .*\)/\1;/;s/^\(Grants for .*\)/## \1 ##/;/##/{x;p;x;}'
 } 
 
@@ -27,7 +30,7 @@ if [ -n "$lastDump" ]; then
         if cmp "$currentDump" "$lastDump" &>/dev/null ;
         then
 #               echo "Files $currentDump and $lastDump are same."
-                if [ "$REMOVEDUPLICATE" -eq 1 ]; then
+                if [ "$removeduplicate" -eq 1 ]; then
                         rm "$currentDump"
                 fi
         fi
@@ -39,34 +42,34 @@ function rotate {
 }
 
 
-if [ -r /etc/default/imt-sql-backup ]; then
-        source /etc/default/imt-sql-backup
-fi
+# if [ -r /etc/default/imt-sql-backup ]; then
+#         source /etc/default/imt-sql-backup
+# fi
 
 #if ! [ -d $(dirname ${CONFIGFILE}) ]; then
 #        # Mysql probably not installed, exiting
 #        exit 0
 #fi
 
-if ! [ -d $OUTPUTDIR ]; then
-        echo "$OUTPUTDIR doesnt exist! I will create."
-        mkdir $OUTPUTDIR
+if ! [ -d $outputdir ]; then
+        echo "$outputdir doesnt exist! I will create."
+        mkdir $outputdir
 fi
 
 # Backup grants
-currentDump="$OUTPUTDIR/grants.$EXTENSION"
+currentDump="$outputdir/grants.$extension"
 mygrants | bzip2 -z > "$currentDump"
-lastDump=$(ls $OUTPUTDIR/grants*.bz2 2>/dev/null | head -1)
+lastDump=$(ls $outputdir/grants*.bz2 2>/dev/null | head -1)
 dedupe "$currentDump" "$lastDump"
 rotate
 
 # get a list of databases
-databases=$(mysql $MYSQL_CONN_STRING -e "SHOW DATABASES;" | tr -d "| " | egrep -v 'Database|information_schema|performance_schema')
+databases=$(mysql $mysql_conn_string -e "SHOW DATABASES;" | tr -d "| " | egrep -v 'Database|information_schema|performance_schema')
 
 #dump each database
 for db in $databases; do
-        currentDump="$OUTPUTDIR/$db.$EXTENSION"
-        mysqldump $MYSQL_CONN_STRING --force --single-transaction --skip-lock-tables --databases $db | bzip2 -z > "$currentDump"
-        lastDump=$(ls $OUTPUTDIR/$db*.bz2 2>/dev/null | head -1)
+        currentDump="$outputdir/$db.$extension"
+        mysqldump $mysql_conn_string --force --single-transaction --skip-lock-tables --databases $db | bzip2 -z > "$currentDump"
+        lastDump=$(ls $outputdir/$db*.bz2 2>/dev/null | head -1)
         dedupe "$currentDump" "$lastDump"
 done
