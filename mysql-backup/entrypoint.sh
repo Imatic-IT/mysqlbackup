862b3a4ea094f6944541c18803442af2cdcaba45
samuel.sabik <samuel.sabik@imatic.cz>
2023-04-21 04:50:33 +0200

    always overwrite cron file

--- mysql-backup/entrypoint.sh
+++ mysql-backup/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # add cron file
-if [ ! -f /etc/cron.d/mysql-backup ]; then
-        # Add cron.d file
-        echo "${MYSQLBACKUP_CRON_TIME} root /etc/imt-sql-backup/mysql-backup.sh >> /var/log/mysql-backup.log 2>&1" > /etc/cron.d/mysql-backup
-fi
+echo "${MYSQLBACKUP_CRON_TIME} root /etc/imt-sql-backup/mysql-backup.sh > /var/log/mysql-backup.log 2>&1" > /etc/cron.d/mysql-backup
 
 # make env variables available in cron jobs
 env > /etc/environment
